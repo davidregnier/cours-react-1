696f6fb4e4b523b1b9c9cbcc7b8d554e70339329
EpicMaDneSss <regnier.david.pro@gmail.com>
2023-07-17 16:45:10 +0200

    jsx first time

---
+++ src/pages/About.js
@@ -0,0 +1,56 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis quam id
+        labore deserunt animi debitis, molestias cupiditate tempore distinctio
+        non aut nemo quisquam provident omnis repellendus sapiente in adipisci
+        molestiae ad quia! Fugit corrupti harum fugiat dolorem recusandae
+        voluptatibus necessitatibus rem eos temporibus magni quasi odio non
+        maxime aliquid, saepe, molestiae, libero consequuntur illum beatae?
+        Tempora, placeat. Nihil eos rem cupiditate sit provident officia ut sint
+        quibusdam voluptatibus, eaque voluptates praesentium quaerat?
+        Voluptatum, inventore eveniet. Quos sit obcaecati incidunt nemo
+        explicabo voluptate ullam perspiciatis magnam! Voluptatibus nostrum quae
+        quas, voluptas itaque corporis id, et inventore totam, architecto
+        doloribus ipsa minima.
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae commodi
+        ad distinctio soluta nulla? Quasi veniam facilis veritatis libero
+        voluptas tempore. Aspernatur sit at distinctio et! Delectus harum dolore
+        quisquam facere fuga dolorum eum modi deserunt provident inventore
+        expedita voluptatum rerum ullam voluptates quaerat numquam, eveniet eius
+        consectetur officia explicabo atque! Eaque in asperiores aspernatur
+        debitis repudiandae eos repellendus soluta. Laborum eos eligendi
+        provident nemo quam numquam dicta qui nostrum impedit, doloribus
+        quibusdam culpa reiciendis rerum, excepturi, magni cumque deleniti
+        explicabo nobis saepe. Mollitia, culpa praesentium! Unde quaerat
+        voluptatibus iusto accusantium dolore laboriosam perferendis temporibus
+        itaque, ipsa quibusdam beatae ad accusamus inventore laborum. Laborum
+        asperiores voluptatibus consequatur distinctio placeat quod provident
+        aliquid, et aut necessitatibus quibusdam vitae ex velit modi nam
+        reiciendis ab animi. Unde, in voluptates itaque id consectetur
+        repudiandae beatae laudantium alias inventore? Dolor veniam ea rem earum
+        nostrum tenetur, repellat impedit eius nesciunt est, atque fugiat quo
+        numquam facere ratione omnis distinctio dolorem velit ipsa, delectus
+        commodi magnam unde laboriosam consequatur. Minima itaque ex deserunt
+        voluptas nulla, id totam voluptatum ratione repellendus esse tenetur
+        saepe ab libero quasi similique? Rerum aliquam incidunt vitae blanditiis
+        similique voluptatem at ab totam, consequatur, reprehenderit, sapiente
+        commodi non in iusto illo.
+      </p>
+    </div>
+  );
+};
+
+export default About;
