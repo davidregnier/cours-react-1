84516e2bccb107818a5429ac45708e09205f014e
EpicMaDneSss <regnier.david.pro@gmail.com>
2023-07-18 15:33:43 +0200

    crud react

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Blog from "./pages/Blog";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/blog" element={<Blog />} />
         {/* path="*" fonctionne si jamais l'url ne correspond à rien de declaré au dessus */}
         <Route path="*" element={<Home />} />
       </Routes>
